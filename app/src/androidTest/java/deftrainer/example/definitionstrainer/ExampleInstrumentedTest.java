7e78b2be95f8d14047f6791ec29d6d3bf5dd9096
david <david-langsam@t-online.de>
2022-05-08 16:36:40 +0200

    Zwei Tests hinzugefügt (wurde auch Zeit)

--- app/src/androidTest/java/deftrainer/example/definitionstrainer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/deftrainer/example/definitionstrainer/ExampleInstrumentedTest.java
@@ -1,14 +1,26 @@
 package deftrainer.example.definitionstrainer;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import deftrainer.example.definitionstrainer.model.Definition;
+import deftrainer.example.definitionstrainer.model.JSONReader;
+import deftrainer.example.definitionstrainer.model.StorageManager;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +29,42 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    Context appContext;
+    List<Definition> definitions_json;
+    List<Definition> definitions_internal;
+
+    @Before
+    public void initTest() {
+        appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        // Try to load internal definitions
+        Object o = StorageManager.loadInternalFile(StorageManager.PATHTODEFINITIONSOBJ, appContext);
+        if (o instanceof List) {
+            definitions_internal = (List<Definition>) o;
+        } else {
+            fail("Unable to load internal definitions!");
+        }
+
+        // try to load external json definitions
+        try {
+            definitions_json = JSONReader.readJsonStream(appContext.getAssets().open(StorageManager.PATHTODEFINITIONSJSON));
+        } catch (IOException ioe2) {
+            fail("Could not read JSON");
+        }
+    }
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.definitionstrainer", appContext.getPackageName());
+    public void assertIdsAreOnyUsedOnce() {
+        Set<Integer> ids = new HashSet<>();
+
+        for(Definition d : definitions_json) {
+            Integer id = d.getID();
+            if(ids.contains(id)) {
+                fail("Definition-ID *"+id+"* was used at least twice!");
+            } else {
+                ids.add(id);
+            }
+        }
     }
 }
